d33deb9c1842b497b44a381a2c44f0feaf30bab8
danylaporte <dany_laporte@hotmail.com>
2015-05-26 17:49:01 +0000

    Improve performance of fromPromise

--- src/fromPromise.js
+++ src/fromPromise.js
@@ -1,39 +1,51 @@
 Asyncplify.fromPromise = function (promise, cb) {
     return new Asyncplify(FromPromise, promise);
-}
+};
 
 function FromPromise(promise, on) {
     this.on = on;
-    this.p = promise;
     this.resolved = 0;
     this.state = RUNNING;
     this.value = null;
 
     on.source = this;
+    
+    var self = this;
+    
+    function resolve(v) {
+        if (self.state === RUNNING) {
+            self.state = CLOSED;
+            self.on.emit(v);
+            self.on.end(null);
+        } else {
+            self.resolved = 1;
+            self.value = v;
+        }
+    }
+    
+    function rejected(err) {
+        if (self.state === RUNNING) {
+            self.state = CLOSED;
+            self.on.end(err);
+        } else {
+            self.resolved = 2;
+            self.value = err;
+        }
+    }
 
-    promise.then(function (v) {
-        this.resolved = 1;
-        this.value = v;
-        this.state === RUNNING && this.do();
-    }.bind(this), function (err) {
-        this.resolved = 2;
-        this.value = err;
-        this.state === RUNNING && this.do();
-    }.bind(this))
+    promise.then(resolve, rejected);
 }
 
 FromPromise.prototype = {
     do: function () {
-        switch (this.resolved) {
-            case 1:
-                this.on.emit(this.value);
-                this.on.end();
-                break;
-            case 2:
-                this.state = CLOSED;
-                this.on.end(this.value);
-                break;
+        if (this.resolved === 1) {
+            this.state = CLOSED;
+            this.on.emit(this.value);
+            this.on.end(null);
+        } else if (this.resolved === 2) {
+            this.state = CLOSED;
+            this.on.end(this.value);
         }
     },
     setState: setState
-}
+};
