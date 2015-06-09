0976b853e56377c304ae14a7d543ce8b9e8c6f4a
danylaporte <dany_laporte@hotmail.com>
2015-06-09 17:06:49 -0400

    add process aborting support to finally

--- src/finally.js
+++ src/finally.js
@@ -7,6 +7,7 @@ function Finally(action, on, source) {
     this.on = on;
     this.source = null;
     this.state = RUNNING;
+    this.registerProcessEnd(true);
 
     on.source = this;
     source._subscribe(this);
@@ -17,15 +18,29 @@ Finally.prototype = {
     end: function (err) {
         if (this.state !== CLOSED) {
             this.state = CLOSED;
+            this.registerProcessEnd(false);
             this.action();
             this.on.end(err);
+        }
+    },
+    registerProcessEnd: function (register) {
+        if (typeof process === 'object') {
+            var func = process[register ? 'on' : 'removeListener'];
+        
+            func('SIGINT', this.action);
+            func('SIGQUIT', this.action);
+            func('SIGTERM', this.action);
         }
     },
     setState: function (state) {
         if (this.state !== state && this.state !== CLOSED) {
             this.state = state;
             this.source.setState(state);
-            if (this.state === state && state === CLOSED) this.action();
+            
+            if (this.state === state && state === CLOSED) {
+                this.registerProcessEnd(false);
+                this.action();
+            }
         }
     }
 };
