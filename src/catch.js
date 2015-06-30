d1fabce63f491701a43b5422a23555efd4ff2b07
danylaporte <dany_laporte@hotmail.com>
2015-06-30 16:47:42 +0000

    refactor catch operator

--- src/catch.js
+++ src/catch.js
@@ -1,36 +1,38 @@
-Asyncplify.prototype.catch = function (options) {
+Asyncplify.prototype.catch = function(options) {
     return new Asyncplify(Catch, options, this);
 };
 
-function Catch(options, on, source) {
-    this.i = 0;
-    this.isSubscriberError = false;
-    this.on = on;
-    this.options = options;
-    this.source = null;
-
-    if (typeof options === 'function') this.mapper = options;
-
-    on.source = this;
+function Catch(options, sink, source) {
+    this._i = 0;
+    this._sink = sink;
+    this._source = null;
+    this._sources = null;
+    if(options && options._mapper) this._mapper = options._mapper;
+    else if(typeof options === 'function') this._mapper = options;
+    else this._sources = options && options.sources || Array.isArray(options) ? options : [];
+    sink._source = this;
     source._subscribe(this);
 }
-
 Catch.prototype = {
-    emit: function (value) {
-        this.isSubscriberError = true;
-        this.on.emit(value);
-        this.isSubscriberError = false;
+    cancel: function() {
+        this._sink = null;
+        if(this._source) this._source.cancel();
     },
-    end: function (err) {
-        if (err && !this.isSubscriberError) {
-            var source = this.mapper(err);
-            if (source) return source._subscribe(this);
+    emit: function(value) {
+        if(this._sink) this._sink.emit(value);
+    },
+    end: function(err) {
+        this._source = null;
+        if(err && this.sink) {
+            var source = this._mapper(err, this._sink);
+            if(source && this._sink) return source._subscribe(this);
         }
-
-        this.on.end(err);
+        if(this._sink) {
+            this._sink.end(null);
+            this._sink = null;
+        }
     },
-    mapper: function () {
-        return this.i < this.options.length && this.options[this.i++];
-    },
-    setState: setStateThru
+    _mapper: function() {
+        return this._i < this._sources.length && this._sources[this._i++];
+    }
 };
