8d5bbea1097db1709c9377c3f9fb48e5398a1fc0
Dany Laporte <dany_laporte@hotmail.com>
2015-07-01 08:06:34 -0400

    refactor TakeWhile operator

--- dist/asyncplify.js
+++ dist/asyncplify.js
@@ -1360,24 +1360,31 @@
     Asyncplify.prototype.takeWhile = function (cond) {
         return new Asyncplify(TakeWhile, cond, this);
     };
-    function TakeWhile(cond, on, source) {
+    function TakeWhile(cond, sink, source) {
         this.cond = cond;
-        this.on = on;
+        this.sink = sink;
+        this.sink.source = this;
         this.source = null;
-        on.source = this;
         source._subscribe(this);
     }
     TakeWhile.prototype = {
+        close: closeSinkSource,
         emit: function (value) {
-            if (this.cond(value)) {
-                this.on.emit(value);
-            } else {
-                this.source.setState(CLOSED);
-                this.on.end(null);
+            if (this.sink && this.cond(value) && this.sink)
+                this.sink.emit(value);
+            else if (this.sink) {
+                var sink = this.sink;
+                var source = this.source;
+                this.sink = null;
+                this.source = null;
+                if (source)
+                    source.close();
+                if (sink)
+                    sink.end(null);
+                ;
             }
         },
-        end: endThru,
-        setState: setStateThru
+        end: endSinkSource
     };
     Asyncplify.prototype.tap = function (options) {
         return new Asyncplify(Tap, options, this);
--- src/takeWhile.js
+++ src/takeWhile.js
@@ -1,25 +1,35 @@
 Asyncplify.prototype.takeWhile = function (cond) {
     return new Asyncplify(TakeWhile, cond, this);
-}
+};
 
-function TakeWhile(cond, on, source) {
+function TakeWhile(cond, sink, source) {
     this.cond = cond;
-    this.on = on;
+    this.sink = sink;
+    this.sink.source = this;
     this.source = null;
 
-    on.source = this;
     source._subscribe(this);
 }
 
 TakeWhile.prototype = {
+    close: closeSinkSource,
     emit: function (value) {
-        if (this.cond(value)) {
-            this.on.emit(value);
-        } else {
-            this.source.setState(CLOSED);
-            this.on.end(null);
+        if (this.sink && this.cond(value) && this.sink)
+            this.sink.emit(value);
+            
+        else if (this.sink) {
+            var sink = this.sink;
+            var source = this.source;
+            
+            this.sink = null;
+            this.source = null;
+            
+            if (source)
+                source.close();
+                
+            if (sink)
+                sink.end(null);;
         }
     },
-    end: endThru,
-    setState: setStateThru
-}
+    end: endSinkSource
+};
