3ff665ea69b404c78288dbf905e188956ddb25b7
danylaporte <dany_laporte@hotmail.com>
2015-07-02 13:58:55 +0000

    Simplify and optimize Timeout

--- src/timeout.js
+++ src/timeout.js
@@ -6,41 +6,53 @@ function Timeout(options, sink, source) {
     this.delay = typeof options === 'number' ? options : options && options.delay || 0;
     this.dueTime = options instanceof Date ? options : options && options.dueTime;
     this.other = options instanceof Asyncplify ? options : (options && options.other || Asyncplify.throw(new Error('Timeout'))); 
-    this.schedulerContext = (options && options.scheduler || schedulers.timeout)();
+    this.scheduler = (options && options.scheduler || schedulers.timeout)();
     this.sink = sink;
     this.sink.source = this;
     this.source = null;
+    this.subscribable = source;
 
-    this.schedulerContext.schedule(this);
-    source._subscribe(this);
+    this.scheduler.schedule(this);
+    
+    if (this.subscribable) {
+        this.subscribable._subscribe(this);
+        this.subscribable = null;
+    }
 }
 
 Timeout.prototype = {
     action: function () {
-        this.closeSource();
-        this.closeSchedulerContext();
+        this.scheduler.close();
+        this.subscribable = this.scheduler = null;
+        if (this.source) this.source.close();
         this.other._subscribe(this);
     },
     close: function () {
-        this.sink = null;
-        this.closeSource();
-        this.closeSchedulerContext();  
+        if (this.source) this.source.close();
+        if (this.scheduler) this.scheduler.close();
+        this.sink = this.scheduler = this.source = null;
     },
-    closeSource: closeSource,
-    closeSchedulerContext: closeSchedulerContext,
     emit: function (value) {
-        this.closeSchedulerContext();
+        if (this.scheduler) this.scheduler.close();
+        this.scheduler = null;
         this.sink.emit(value);
     },
     end: function (err) {
-        this.source = null;
-        this.closeSchedulerContext();
-        this.endSink(err);
+        if (this.scheduler) this.scheduler.close();
+        this.source = this.scheduler = null;
+        
+        var sink = this.sink;
+        this.sink = null;
+        if (sink) sink.end(err);
     },
-    endSink: endSink,
     error: function (err) {
-        this.closeSource();
-        this.closeSchedulerContext();
-        this.endSink(err);
+        this.scheduler.close();
+        this.scheduler = null;
+        
+        if (this.source) this.source.close();
+        this.source = null;
+        
+        if (this.sink) this.sink.end(err);
+        this.sink = null;
     }
 };
