c2b3ea45883d5a449101ab17097a045c59b6fc7d
danylaporte <dany_laporte@hotmail.com>
2015-07-02 18:50:08 +0000

    refactor scan operator

--- src/scan.js
+++ src/scan.js
@@ -1,26 +1,33 @@
 Asyncplify.prototype.scan = function (options, source, cb) {
-    return new Asyncplify(Scan, options, this)
-}
+    return new Asyncplify(Scan, options, this);
+};
 
 function scanIdentity(acc, v) {
     return (acc || 0) + (v || 0);
 }
 
-function Scan(options, on, source) {
-    this.mapper = typeof options === 'function' ? options : (options && options.mapper || scanIdentity);
+function Scan(options, sink, source) {
     this.acc = options && options.initial || 0;
-    this.on = on;
+    this.mapper = typeof options === 'function' ? options : (options && options.mapper || scanIdentity);
+    this.sink = sink;
+    this.sink.source = this;
     this.source = null;
 
-    on.source = this;
     source._subscribe(this);
 }
 
 Scan.prototype = {
+    close: function () {
+        if (this.source) this.source.close();
+        this.source = null;
+    },
     emit: function (value) {
         this.acc = this.mapper(this.acc, value);
-        this.on.emit(this.acc);
+        this.sink.emit(this.acc);
     },
-    end: endThru,
-    setState: setStateThru
-}
+    end: function (err) {
+        this.mapper = null;
+        this.source = null;
+        this.sink.end(err);
+    }
+};
